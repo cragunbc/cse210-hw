919904ca46fb553447128ccc14bfd201e02bfaf0
cragunbc <bccragun@yahoo.com>
2023-09-16 14:48:04 -0600

    Second Prep assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        int percentage = int.Parse(number);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (percentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade letter is a: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the course. Please try again.");
+        }
     }
 }
