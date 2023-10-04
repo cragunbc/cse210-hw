a34e03037b72b386874071096e09b5b6b0d0607b
cragunbc <bccragun@yahoo.com>
2023-10-03 19:42:08 -0600

    Week 4 Learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Brantley", "Science");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Brantley", "Science", "27", "3");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+ 
+        WritingAssignment w1 = new WritingAssignment("Brantley", "World History", "The World History 101");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+
     }
 }
